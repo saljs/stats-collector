5057098ada4665a11987710a837107b034b778dd
Salvatore Skare <salskare@gmail.com>
2023-08-03 11:06:46 -0500

    Fixed backup script to be compatable with alpine.

--- backups/backup-db.sh
+++ backups/backup-db.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 # Run mysqldump to export all databases from server
 mysqldump --host=$DB_HOST --port=$DB_PORT \
     --user=$DB_USER --password=$DB_PASSWORD \
-    --no-create-info vivmonitors \
-    > "$OUT_PATH/vivmonitors_backup_$(date +%FT%T).sql"
+    --all-databases > "${OUT_PATH}/backup_$(date +%FT%T).sql"
 
 # Delete all backups older than KEEP_DAYS days
 find $OUT_PATH -mtime +$KEEP_DAYS -exec rm {} \;
