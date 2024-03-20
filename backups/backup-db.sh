9bfbf3ed482d45de97f59b5543fd466026a4d711
Salvatore Skare <salskare@gmail.com>
2024-03-20 11:55:34 -0500

    Well I finally have an actual x86 server ready to go, so I've made a new docker compose script and removed some of the armv6 jankiness.

--- backups/backup-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run mysqldump to export all databases from server
-mysqldump --host=$DB_HOST --port=$DB_PORT \
-    --user=$DB_USER --password=$DB_PASSWORD \
-    --all-databases > "${OUT_PATH}/backup_$(date +%FT%T).sql"
-
-# Delete all backups older than KEEP_DAYS days
-find $OUT_PATH -mtime +$KEEP_DAYS -exec rm {} \;
